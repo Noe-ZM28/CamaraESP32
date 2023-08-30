439fb9eeaf5fde32d67919f20d62c3d336a4c702
Raspberry - Noe <sistemas@pasesa.com>
2023-08-30 15:41:13 -0600

    Se agrega función para buscar cuadros para identificar la placa

--- cam_esp32.py
+++ cam_esp32.py
@@ -2,7 +2,7 @@ import cv2
 import pytesseract
 
 # Cargar la imagen desde el archivo
-image = cv2.imread('c.jpg')
+image = cv2.imread('a.jpg')
 
 # Convertir la imagen a escala de grises
 gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
@@ -16,39 +16,30 @@ edges = cv2.Canny(blurred_image, threshold1=50, threshold2=150)
 # Encontrar los contornos en la imagen
 contours, _ = cv2.findContours(edges.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-# Ordenar los contornos por área de mayor a menor
-contours = sorted(contours, key=cv2.contourArea, reverse=True)[:10]
-
-# Inicializar una variable para el contorno de la placa
-plate_contour = None
-
-# Recorrer los contornos en busca de la placa
+# Recorrer los contornos en busca de rectángulos
 for contour in contours:
     perimeter = cv2.arcLength(contour, True)
-    approx = cv2.approxPolyDP(contour, 0.02 * perimeter, True)
+    approx = cv2.approxPolyDP(contour, 0.04 * perimeter, True)
     
+    # Si el contorno tiene cuatro vértices (rectángulo)
     if len(approx) == 4:
-        plate_contour = approx
-        break
+        x, y, w, h = cv2.boundingRect(approx)
+        
+        # Descartar rectángulos muy pequeños (ajusta este valor según tus necesidades)
+        if w > 100 and h > 20:
+            # Recortar y procesar el área de la placa
+            plate_image = gray_image[y:y + h, x:x + w]
+            
+            # Aplicar OCR con Tesseract al área de la placa
+            plate_text = pytesseract.image_to_string(plate_image, config='--psm 8')
+            
+            # Dibujar el rectángulo del área de la placa en la imagen original
+            cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            
+            # Dibujar el texto de la placa en la imagen original
+            cv2.putText(image, 'Placa: ' + plate_text, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
 
-# Si se encontró el contorno de la placa
-if plate_contour is not None:
-    # Dibujar el contorno de la placa en la imagen original
-    cv2.drawContours(image, [plate_contour], -1, (0, 255, 0), 2)
-    
-    # Recortar y procesar el área de la placa
-    x, y, w, h = cv2.boundingRect(plate_contour)
-    plate_image = gray_image[y:y + h, x:x + w]
-    
-    # Aplicar OCR con Tesseract al área de la placa
-    plate_text = pytesseract.image_to_string(plate_image, config='--psm 8')
-    
-    # Imprimir el texto de la placa
-    print("Texto de la placa:", plate_text)
-    
-    # Mostrar la imagen con el contorno y el texto de la placa
-    cv2.imshow('Placa con Contorno y Texto', image)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
-else:
-    print("No se pudo encontrar la placa en la imagen.")
+# Mostrar la imagen con los rectángulos y el texto de las placas
+cv2.imshow('Imagen con Placas', image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
