60bd1e7d496e7774b7b634b0478b1a62999ee141
Raspberry - Noe <sistemas@pasesa.com>
2023-08-31 11:52:07 -0600

    se modifica para que lea la información de un recuadro

--- cam_esp32.py
+++ cam_esp32.py
@@ -1,13 +1,13 @@
 import cv2
 import pytesseract
 
-# Cargar el video desde el archivo 
+# Cargar el video desde el archivo
 video_path = 'video.mp4'
 cap = cv2.VideoCapture(video_path)
 
 reader = pytesseract.image_to_string
 
-real_plate=None
+real_plate = None
 
 PLATES = ["A-522-JME", "A522JME", "A 522 JME"]
 
@@ -17,18 +17,29 @@ while cap.isOpened():
     if not ret:
         print("error")
         continue
-        #break
 
-    # Convertir el frame a escala de grises
-    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    # Dimensiones y posición del recuadro rojo en el centro
+    rect_width = 600
+    rect_height = 300
+    rect_x = (frame.shape[1] - rect_width) // 2
+    rect_y = (frame.shape[0] - rect_height) // 2
+
+    # Dibujar el recuadro rojo en el centro del frame
+    cv2.rectangle(frame, (rect_x, rect_y), (rect_x + rect_width, rect_y + rect_height), (0, 0, 255), 2)
+
+    # Recortar el área de interés (rectángulo rojo) de la imagen
+    roi = frame[rect_y:rect_y + rect_height, rect_x:rect_x + rect_width]
+
+    # Convertir el área de interés a escala de grises
+    gray_roi = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
 
     # Aplicar un filtro gaussiano para reducir el ruido y mejorar la detección del texto
-    blurred_frame = cv2.GaussianBlur(gray_frame, (5, 5), 0)
+    blurred_roi = cv2.GaussianBlur(gray_roi, (5, 5), 0)
 
     # Aplicar la detección de bordes con Canny
-    edges = cv2.Canny(blurred_frame, threshold1=50, threshold2=150)
+    edges = cv2.Canny(blurred_roi, threshold1=50, threshold2=150)
 
-    # Encontrar los contornos en el frame
+    # Encontrar los contornos en el área de interés
     contours, _ = cv2.findContours(edges.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
     # Recorrer los contornos en busca de rectángulos
@@ -40,14 +51,13 @@ while cap.isOpened():
         if len(approx) == 4:
             x, y, w, h = cv2.boundingRect(approx)
             if w < h:
-                # print("es mas largo que ancho")
                 continue
 
             # Descartar rectángulos muy pequeños y los que sean más largos que anchos
             aspect_ratio = w / float(h)
             if (w > 150 and h > 50) and (aspect_ratio <= 2.5) and (aspect_ratio >= 0.5):
                 # Recortar y procesar el área de la placa
-                plate_image = gray_frame[y:y + h, x:x + w]
+                plate_image = gray_roi[y:y + h, x:x + w]
 
                 # Aplicar OCR con Tesseract al área de la placa
                 plate_text = reader(plate_image, config='--psm 8')
@@ -56,24 +66,24 @@ while cap.isOpened():
                 if long_plate_text < 5:
                     continue
 
-                # Dibujar el rectángulo del área de la placa en el frame original
-                cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+                # Dibujar el rectángulo del área de la placa en el recuadro rojo
+                cv2.rectangle(frame, (rect_x + x, rect_y + y), (rect_x + x + w, rect_y + y + h), (0, 255, 0), 2)
 
-                # Dibujar el texto de la placa en el frame original
-                cv2.putText(frame, 'Placa: ' + plate_text, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+                # Dibujar el texto de la placa sobre el recuadro rojo
+                cv2.putText(frame, 'Placa: ' + plate_text, (rect_x + x, rect_y + y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
 
                 if plate_text in PLATES:
-                    print("*"*50)
+                    print("*" * 50)
                     print("Funciona!")
                 print(plate_text)
 
     # Redimensionar el frame para mostrarlo en una ventana más pequeña
     resized_frame = cv2.resize(frame, (640, 480))
 
-    # Mostrar el frame redimensionado con los rectángulos y el texto de las placas
+    # Mostrar el frame con el recuadro rojo, rectángulos y el texto de las placas
     cv2.imshow('Video con Placas', resized_frame)
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    if cv2.waitKey(10) & 0xFF == ord('q'):
         break
 
 cap.release()
