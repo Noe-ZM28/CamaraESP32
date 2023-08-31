97652545477f32410860f56d26080260c3aa2226
Raspberry - Noe <sistemas@pasesa.com>
2023-08-30 19:17:39 -0600

    se convierte en  para aceptar viudeo

--- cam_esp32.py
+++ cam_esp32.py
@@ -1,45 +1,57 @@
 import cv2
 import pytesseract
 
-# Cargar la imagen desde el archivo
-image = cv2.imread('a.jpg')
+# Cargar el video desde el archivo (ajusta la ruta según tu archivo)
+video_path = 'video.mp4'
+cap = cv2.VideoCapture(video_path)
 
-# Convertir la imagen a escala de grises
-gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+reader = pytesseract.image_to_string
 
-# Aplicar un filtro gaussiano para reducir el ruido y mejorar la detección del texto
-blurred_image = cv2.GaussianBlur(gray_image, (5, 5), 0)
+while cap.isOpened():
+    ret, frame = cap.read()
+    if not ret:
+        break
 
-# Aplicar la detección de bordes con Canny
-edges = cv2.Canny(blurred_image, threshold1=50, threshold2=150)
+    # Convertir el frame a escala de grises
+    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-# Encontrar los contornos en la imagen
-contours, _ = cv2.findContours(edges.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    # Aplicar un filtro gaussiano para reducir el ruido y mejorar la detección del texto
+    blurred_frame = cv2.GaussianBlur(gray_frame, (5, 5), 0)
 
-# Recorrer los contornos en busca de rectángulos
-for contour in contours:
-    perimeter = cv2.arcLength(contour, True)
-    approx = cv2.approxPolyDP(contour, 0.04 * perimeter, True)
-    
-    # Si el contorno tiene cuatro vértices (rectángulo)
-    if len(approx) == 4:
-        x, y, w, h = cv2.boundingRect(approx)
-        
-        # Descartar rectángulos muy pequeños (ajusta este valor según tus necesidades)
-        if w > 100 and h > 20:
-            # Recortar y procesar el área de la placa
-            plate_image = gray_image[y:y + h, x:x + w]
-            
-            # Aplicar OCR con Tesseract al área de la placa
-            plate_text = pytesseract.image_to_string(plate_image, config='--psm 8')
-            
-            # Dibujar el rectángulo del área de la placa en la imagen original
-            cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
-            
-            # Dibujar el texto de la placa en la imagen original
-            cv2.putText(image, 'Placa: ' + plate_text, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+    # Aplicar la detección de bordes con Canny
+    edges = cv2.Canny(blurred_frame, threshold1=50, threshold2=150)
 
-# Mostrar la imagen con los rectángulos y el texto de las placas
-cv2.imshow('Imagen con Placas', image)
-cv2.waitKey(0)
+    # Encontrar los contornos en el frame
+    contours, _ = cv2.findContours(edges.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    # Recorrer los contornos en busca de rectángulos
+    for contour in contours:
+        perimeter = cv2.arcLength(contour, True)
+        approx = cv2.approxPolyDP(contour, 0.04 * perimeter, True)
+
+        # Si el contorno tiene cuatro vértices (rectángulo)
+        if len(approx) == 4:
+            x, y, w, h = cv2.boundingRect(approx)
+
+            # Descartar rectángulos muy pequeños
+            if w > 150 and h > 20:
+                # Recortar y procesar el área de la placa
+                plate_image = gray_frame[y:y + h, x:x + w]
+
+                # Aplicar OCR con Tesseract al área de la placa
+                plate_text = reader(plate_image, config='--psm 8')
+
+                # Dibujar el rectángulo del área de la placa en el frame original
+                cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+                # Dibujar el texto de la placa en el frame original
+                cv2.putText(frame, 'Placa: ' + plate_text, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+
+    # Mostrar el frame con los rectángulos y el texto de las placas
+    cv2.imshow('Video con Placas', frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
 cv2.destroyAllWindows()
