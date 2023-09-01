8fa9048dc90c514391cb42f87923bc06d4dabf37
Raspberry - Noe <sistemas@pasesa.com>
2023-09-01 14:01:58 -0600

    Feat: Reconocimiento de texto

--- cam_esp32.py
+++ cam_esp32.py
@@ -13,8 +13,8 @@ PLATES = ["A-522-JME", "LD-73-546", "LF-55-593"]
 
 window_size = (1040, 680)
 
-rect_width = 600
-rect_height = 300
+rect_width = 300
+rect_height = 150
 
 
 bytes = bytes()
@@ -26,7 +26,7 @@ while True:
         jpg = bytes[a:b+2]
         bytes = bytes[b+2:]
         if jpg :
-            frame = cv2.imdecode(np.fromstring(jpg, dtype=np.uint8), cv2.IMREAD_COLOR)
+            frame = cv2.imdecode(np.frombuffer(jpg, dtype=np.uint8), cv2.IMREAD_COLOR)
 
 
             # Dimensiones y posición del recuadro rojo en el centro
@@ -35,6 +35,65 @@ while True:
 
             # Dibujar el recuadro rojo en el centro del frame
             cv2.rectangle(frame, (rect_x, rect_y), (rect_x + rect_width, rect_y + rect_height), (0, 0, 255), 2)
+
+            # Recortar el área de interés (rectángulo rojo) de la imagen
+            roi = frame[rect_y:rect_y + rect_height, rect_x:rect_x + rect_width]
+
+            # Convertir el área de interés a escala de grises
+            gray_roi = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
+
+            # Aplicar un filtro gaussiano para reducir el ruido y mejorar la detección del texto
+            blurred_roi = cv2.GaussianBlur(gray_roi, (5, 5), 0)
+
+            # Aplicar la detección de bordes con Canny
+            edges = cv2.Canny(blurred_roi, threshold1=100, threshold2=200)
+
+            # Encontrar los contornos en el área de interés
+            contours, _ = cv2.findContours(edges.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+            # Variable para rastrear si se ha detectado un rectángulo en este frame
+            rectangle_detected = False
+
+            # Recorrer los contornos en busca de rectángulos
+            for contour in contours:
+                perimeter = cv2.arcLength(contour, True)
+                approx = cv2.approxPolyDP(contour, 0.04 * perimeter, True)
+                if rectangle_detected:
+                    break  # Salir del bucle si ya se ha detectado un rectángulo
+
+                # Si el contorno tiene cuatro vértices (rectángulo)
+                if len(approx) !=4:
+                    continue
+
+                x, y, w, h = cv2.boundingRect(approx)
+
+                if w < h:
+                    continue
+
+                aspect_ratio = w / float(h)
+                if (w > 150 and h > 50) and (aspect_ratio <= 2.5) and (aspect_ratio >= 1):
+
+                    # Recortar y procesar el área de la placa
+                    plate_image = gray_roi[y:y + h, x:x + w]
+
+                    # Aplicar OCR con Tesseract al área de la placa
+                    plate_text = reader(plate_image, config='--psm 8')
+                    long_plate_text = len(plate_text)
+
+                    # #Si la cantidad de letras detectadas es menor a 8 pasa al siguiene Frame
+                    # if long_plate_text < 8:
+                    #     continue
+
+                    # Dibujar el rectángulo del área de la placa en el recuadro rojo
+                    cv2.rectangle(frame, (rect_x + x, rect_y + y), (rect_x + x + w, rect_y + y + h), (0, 255, 0), 2)
+
+                    # Dibujar el texto de la placa sobre el recuadro rojo
+                    cv2.putText(frame, f'Texto: {plate_text}', (rect_x, rect_y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+                    real_txt_plate = plate_text
+
+                    print(plate_text)
+                    rectangle_detected = True
+
 
             # Redimensionar el frame para mostrarlo en una ventana más pequeña
             resized_frame = cv2.resize(frame, window_size)
