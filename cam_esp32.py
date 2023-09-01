889363cd2ea495cfee7b830601aac98ce3d2ad9b
Raspberry - Noe <sistemas@pasesa.com>
2023-09-01 13:42:55 -0600

    Feat: se redimenciona la ventana y se añade area de interes

--- cam_esp32.py
+++ cam_esp32.py
@@ -1,8 +1,22 @@
 import cv2
 from urllib.request import urlopen
 import numpy as np
+import pytesseract
  
 stream = urlopen('http://192.168.1.227:81/stream')
+
+reader = pytesseract.image_to_string
+
+real_txt_plate = None
+
+PLATES = ["A-522-JME", "LD-73-546", "LF-55-593"]
+
+window_size = (1040, 680)
+
+rect_width = 600
+rect_height = 300
+
+
 bytes = bytes()
 while True:
     bytes += stream.read(1024)
@@ -13,7 +27,20 @@ while True:
         bytes = bytes[b+2:]
         if jpg :
             frame = cv2.imdecode(np.fromstring(jpg, dtype=np.uint8), cv2.IMREAD_COLOR)
-            cv2.imshow('ESP32 CAM', frame)
+
+
+            # Dimensiones y posición del recuadro rojo en el centro
+            rect_x = (frame.shape[1] - rect_width) // 2
+            rect_y = (frame.shape[0] - rect_height) // 2
+
+            # Dibujar el recuadro rojo en el centro del frame
+            cv2.rectangle(frame, (rect_x, rect_y), (rect_x + rect_width, rect_y + rect_height), (0, 0, 255), 2)
+
+            # Redimensionar el frame para mostrarlo en una ventana más pequeña
+            resized_frame = cv2.resize(frame, window_size)
+
+            # Mostrar el frame con el recuadro rojo, rectángulos y el texto de las placas
+            cv2.imshow('ESP32 CAM', resized_frame)
     if cv2.waitKey(1) & 0xff == ord('q'):
         break
 stream.release()
