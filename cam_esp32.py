59746ed155dc9f39c4787e4ef4b64682cacede9e
Raspberry - Noe <sistemas@pasesa.com>
2023-09-04 12:51:00 -0600

    Revert "Feat: se modifica tamaño de area de interes al tamaño del frame"
    This reverts commit 204801e6f26b39577aa143e4174c8e86125eb6b5.

--- cam_esp32.py
+++ cam_esp32.py
@@ -1,16 +1,16 @@
-from cv2 import imdecode, IMREAD_COLOR, COLOR_BGR2GRAY, cvtColor, GaussianBlur, filter2D, Canny, findContours, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE, arcLength, approxPolyDP, boundingRect, rectangle, putText, FONT_HERSHEY_SIMPLEX, imshow, waitKey, destroyAllWindows
-
+import cv2
 from urllib.request import urlopen
 import numpy as np
 import pytesseract
-#Reemplazar por la ip
-url = "http://192.168.1.160:81/stream"
-
-stream = urlopen(url)
+ 
+# stream = urlopen('http://192.168.1.227:81/stream') # IP OFICINA
+stream = urlopen('http://192.168.1.160:81/stream') # IP CASA
 
 reader = pytesseract.image_to_string
 
 real_txt_plate = None
+
+window_size = (320, 240)
 
 rect_width = 175
 rect_height = 100
@@ -28,37 +28,86 @@ while True:
         bytes = bytes[b+2:]
         if not jpg:
             continue
-        frame = imdecode(np.frombuffer(jpg, dtype=np.uint8), IMREAD_COLOR)
-        gray_roi = cvtColor(frame, COLOR_BGR2GRAY)
-        blurred_roi = GaussianBlur(gray_roi, (5, 5), 0)
-        sharpened_roi = filter2D(blurred_roi, -1, np.array([[-1, -1, -1], [-1, 9, -1], [-1, -1, -1]]))
-        edges = Canny(sharpened_roi, threshold1=100, threshold2=200)
-        contours, _ = findContours(edges.copy(), RETR_EXTERNAL, CHAIN_APPROX_SIMPLE)
+
+        frame = cv2.imdecode(np.frombuffer(jpg, dtype=np.uint8), cv2.IMREAD_COLOR)
+
+        # # Dimensiones y posición del recuadro rojo en el centro
+        # rect_x = (frame.shape[1] - rect_width) // 2
+        # rect_y = (frame.shape[0] - rect_height) // 2
+
+        # Dibujar el recuadro rojo en el centro del frame
+        cv2.rectangle(frame, (rect_x, rect_y), (rect_x + rect_width, rect_y + rect_height), (0, 0, 255), 2)
+
+        # Recortar el área de interés (rectángulo rojo) de la imagen
+        roi = frame[rect_y:rect_y + rect_height, rect_x:rect_x + rect_width]
+
+        # Convertir el área de interés a escala de grises
+        gray_roi = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
+
+        # Aplicar un filtro gaussiano para reducir el ruido y mejorar la detección del texto
+        blurred_roi = cv2.GaussianBlur(gray_roi, (5, 5), 0)
+
+        # Aplicar un filtro de nitidez para mejorar la nitidez de la imagen
+        sharpened_roi = cv2.filter2D(blurred_roi, -1, np.array([[-1, -1, -1], [-1, 9, -1], [-1, -1, -1]]))
+
+        # Aplicar la detección de bordes con Canny
+        edges = cv2.Canny(sharpened_roi, threshold1=100, threshold2=200)
+
+        # Encontrar los contornos en el área de interés
+        contours, _ = cv2.findContours(edges.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+        # Variable para rastrear si se ha detectado un rectángulo en este frame
         rectangle_detected = False
+
+        # Recorrer los contornos en busca de rectángulos
         for contour in contours:
-            perimeter = arcLength(contour, True)
-            approx = approxPolyDP(contour, 0.04 * perimeter, True)
-            if rectangle_detected:break
-            if len(approx) != 4:continue
-            x, y, w, h = boundingRect(approx)
-            if w < h:continue
+            perimeter = cv2.arcLength(contour, True)
+            approx = cv2.approxPolyDP(contour, 0.04 * perimeter, True)
+            if rectangle_detected:
+                break  # Salir del bucle si ya se ha detectado un rectángulo
+
+            # Si el contorno tiene cuatro vértices (rectángulo)
+            if len(approx) != 4:
+                continue
+
+            x, y, w, h = cv2.boundingRect(approx)
+
+            if w < h:
+                continue
 
             aspect_ratio = w / float(h)
             if (w > 150 and h > 50) and (aspect_ratio <= 2.5) and (aspect_ratio >= 1):
+                # Recortar y procesar el área de la placa
                 plate_image = gray_roi[y:y + h, x:x + w]
+
+                # Aplicar OCR con Tesseract al área de la placa
                 plate_text = reader(plate_image, config='--psm 8')
                 long_plate_text = len(plate_text)
+
+                #Si la cantidad de letras detectadas es menor a 8 pasa al siguiene Frame
                 if long_plate_text < 5:
                     continue
-                rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-                putText(frame, f'Texto: {plate_text}', (rect_x, rect_y - 10), FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+
+                # Dibujar el rectángulo del área de la placa en el recuadro rojo
+                cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+                # Dibujar el texto de la placa sobre el recuadro rojo
+                cv2.putText(frame, f'Texto: {plate_text}', (rect_x, rect_y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
 
                 real_txt_plate = str(plate_text)
 
                 print(real_txt_plate)
                 rectangle_detected = True
-        imshow('ESP32 CAM', frame)
-    if waitKey(1) & 0xFF == ord('q'):break #Precionar Q para salir
 
-destroyAllWindows()
+        # Redimensionar el frame para mostrarlo en una ventana más pequeña
+        resized_frame = cv2.resize(frame, window_size)
+
+        # Mostrar el frame con el recuadro rojo, rectángulos y el texto de las placas
+        cv2.imshow('ESP32 CAM', frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cv2.destroyAllWindows()
+
 
