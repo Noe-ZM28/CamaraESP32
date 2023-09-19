0c958858f5a8ba33d458d7603f5793aae243922b
Raspberry - Noe <sistemas@pasesa.com>
2023-09-19 10:46:03 -0600

    cambio a procesamiento de imagen

--- process_image/process_frame.py
+++ process_image/process_frame.py
@@ -7,7 +7,6 @@ class ProcessFrame:
     def process_frame(self,
             frame: ndarray,
             show: bool = False,
-            count_gasussian = 1,
             sharpen_kernel: ndarray = np.array([[-1, -1, -1],
                                                 [-1, 9, -1],
                                                 [-1, -1, -1]]),
@@ -28,18 +27,15 @@ class ProcessFrame:
 
         # Aplicar un filtro gaussiano para reducir el ruido y mejorar la detección del texto
         blurred_roi = cv2.GaussianBlur(gray_roi, (5, 5), 0)
-
-        if count_gasussian > 1:
-            for i in range(count_gasussian):
-                blurred_roi = cv2.GaussianBlur(blurred_roi, (5, 5), 0)
-
+        blurred_roi = cv2.GaussianBlur(blurred_roi, (5, 5), 0)
 
         # Aplicar un filtro de nitidez para mejorar la nitidez de la imagen
         sharpened_roi = cv2.filter2D(blurred_roi, -1, sharpen_kernel)
+
         frame_proceed = sharpened_roi
 
         # Aplicar la detección de bordes con Canny
-        edges = cv2.Canny(sharpened_roi, threshold1=canny_threshold1, threshold2=canny_threshold2)
+        edges = cv2.Canny(frame_proceed, threshold1=canny_threshold1, threshold2=canny_threshold2)
 
         # Encontrar los contornos en el área de interés
         contours, _ = cv2.findContours(edges.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
